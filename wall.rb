5a58b73bb09a1be9accf29d1c674cf0d86bf95ef
piotrplaneta <piotr.planeta@gmail.com>
2013-01-31 23:07:32 +0100

    indentation fix

--- wall.rb
+++ wall.rb
@@ -1,22 +1,22 @@
 class Wall
-	attr_accessor :x, :y, :texture, :space_start
+  attr_accessor :x, :y, :texture, :space_start
 
-	def initialize(width, space_width, x, y)
-		@width, @space_width, @x, @y = width, space_width, x, y
-		@space_start = rand(0..(width - space_width))
+  def initialize(width, space_width, x, y)
+    @width, @space_width, @x, @y = width, space_width, x, y
+    @space_start = rand(0..(width - space_width))
 
-		generate_texture
-	end
+    generate_texture
+  end
 
-	def generate_texture
-		texture = '#' * @width
+  def generate_texture
+    texture = '#' * @width
 
-		texture[@space_start, @space_width] = " " * @space_width
+    texture[@space_start, @space_width] = " " * @space_width
 
-		@texture = [texture]
-	end
+    @texture = [texture]
+  end
 
-	def move_up
-		self.y -= 1
-	end
+  def move_up
+    self.y -= 1
+  end
 end
